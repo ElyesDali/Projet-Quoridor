e6d3004e14d69f2b0bfe1da80e156e91d902f73c
Zaki-Nacer <zaki.nacer@edu.ece.fr>
2024-11-21 00:51:12 +0100

    Update Barriere.h

--- Barriere.h
+++ Barriere.h
@@ -1,9 +1,13 @@
-//
-// Created by joshu on 20/11/2024.
-//
-
 #ifndef BARRIERE_H
 #define BARRIERE_H
+
+typedef struct {
+    int x;
+    int y;
+    char type; // 'H' pour horizontale, 'V' pour verticale
+} Barriere;
+
 int estBarrierePosee(int x, int y, char type);
 void poserBarriere(int x, int y, char type);
-#endif //BARRIERE_H
+
+#endif // BARRIERE_H
