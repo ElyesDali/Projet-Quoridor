982e490c4fc805673c0dff09695668ee9567aca9
Zaki-Nacer <zaki.nacer@edu.ece.fr>
2024-11-21 00:52:48 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#include "Plato.h"
+#include "deplacement.h"
+
+#include <stdio.h>
+#include "Plato.h"
+#include "deplacement.h"
+
+void gameLoop() {
+    int playerX = 0, playerY = 4; // Initial position of player 1
+    int player2X = 9, player2Y = 4; // Initial position of player 2
+    int currentPlayer = 1;
+
+    while (1) {
+        afficherPlateau();
+        printf("\nPlayer %d's turn. Enter command (0 to quit): ", currentPlayer);
+        int command;
+        scanf("%d", &command);
+        if (command == 0) {
+            break;
+        }
+
+        if (currentPlayer == 1) {
+            gererDeplacement(&playerX, &playerY, currentPlayer);
+            currentPlayer = 2;
+        } else {
+            gererDeplacement(&player2X, &player2Y, currentPlayer);
+            currentPlayer = 1;
+        }
+    }
+}
 
 int menu() {
     int choix = 0;
@@ -15,80 +45,86 @@ int menu() {
 
         scanf("%d", &choix);
 
-        if (choix == 1) {
-            printf("\nCHOISIR LE MODE DE JEU\n");
-            printf("\n1 : Normal\n");
-            printf("2 : Blitz\n");
-            printf("3 : Retour \n");
-            printf("\nChoix : ");
-            scanf("%d", &choix);
+        switch (choix) {
+            case 1: // Jouer
+                printf("\nCHOISIR LE MODE DE JEU\n");
+                printf("\n1 : Normal\n");
+                printf("2 : Blitz\n");
+                printf("3 : Retour\n");
+                printf("\nChoix : ");
+                scanf("%d", &choix);
 
-            if (choix == 1) {
-                printf("Mode Normal sélectionné.\n");
-            }
-            else if (choix == 2) {
-                printf("Mode Blitz sélectionné.\n");
-            }
-            else if (choix == 3) {
-                continue;
-            }
-            else {
+                switch (choix) {
+                    case 1:
+                    case 2:
+                        printf("Mode sélectionné.\n");
+                        initialiserPlateau();
+                        gameLoop();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        printf("Choix invalide.\n");
+                }
+                break;
+
+            case 2: // Reprendre une partie
+                printf("1 : Reprendre une partie\n");
+                printf("2 : Retour\n");
+                printf("\nChoix : ");
+                scanf("%d", &choix);
+                switch (choix) {
+                    case 1:
+                        printf("Reprendre une partie.\n");
+                        initialiserPlateau();
+                        gameLoop();
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        printf("Choix invalide.\n");
+                }
+                break;
+
+            case 3: // Aide
+                printf("1 : Règles à mettre après\n");
+                printf("2 : Retour\n");
+                printf("\nChoix : ");
+                scanf("%d", &choix);
+                switch (choix) {
+                    case 1:
+                        printf("Règles à mettre après.\n");
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        printf("Choix invalide.\n");
+                }
+                break;
+
+            case 4: // Tableau des scores
+                printf("1 : Tableau des scores\n");
+                printf("2 : Retour\n");
+                printf("\nChoix : ");
+                scanf("%d", &choix);
+                switch (choix) {
+                    case 1:
+                        printf("Tableau des scores.\n");
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        printf("Choix invalide.\n");
+                }
+                break;
+
+            case 5: // Quitter
+                printf("Quitter le jeu.\n");
+                return 0;
+
+            default: // Choix invalide
                 printf("Choix invalide.\n");
-            }
-        }
-        else if (choix == 2) {
-            printf("1 : Reprendre une partie.\n");
-            printf(" 2 : Retour\n");
-            printf("\nChoix : ");
-            scanf("%d", &choix);
-            if (choix == 1) {
-                printf("Reprendre une partie.\n");
-            }
-            else if (choix == 2) {
-                continue;
-            }
-            else {
-                printf("Choix invalide.\n");
-            }
-        }
-        else if (choix == 3) {
-            printf("1 : Règles à mettre après.\n");
-            printf(" 2 : Retour\n");
-            printf("\nChoix : ");
-            scanf("%d", &choix);
-            if (choix == 1) {
-                printf("Règles à mettre après.\n");
-            }
-            else if (choix == 2) {
-                continue;
-            }
-            else {
-                printf("Choix invalide.\n");
-            }
-        }
-        else if (choix == 4) {
-            printf("1 : Tableau des scores.\n");
-            printf(" 2 : Retour\n");
-            printf("\nChoix : ");
-            scanf("%d", &choix);
-            if (choix == 1) {
-                printf("Tableau des scores.\n");
-            }
-            else if (choix == 2) {
-                continue;
-            }
-            else {
-                printf("Choix invalide.\n");
-            }
-        }
-        else if (choix == 5) {
-            printf("Quitter le jeu.\n");
-            return 0;
-        }
-        else {
-            printf("Choix invalide.\n");
         }
     }
-
     return 0;
 }
