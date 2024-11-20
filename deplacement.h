d14f63ba06733cec91cd720ef206fcb6dcaf0283
Zaki-Nacer <zaki.nacer@edu.ece.fr>
2024-11-21 00:51:50 +0100

    Update deplacement.h

--- deplacement.h
+++ deplacement.h
@@ -1,9 +1,6 @@
-//
-// Created by joshu on 20/11/2024.
-//
-
 #ifndef DEPLACEMENT_H
 #define DEPLACEMENT_H
-void gererDeplacement(int *playerX, int *playerY);
-void deplacerJoueur(int *x, int *y, int dx, int dy);
-#endif //DEPLACEMENT_H
+
+void gererDeplacement(int *playerX, int *playerY, int currentPlayer);
+
+#endif // DEPLACEMENT_H
