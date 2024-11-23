f85a81ad72ebe4b6fc6f8510858c345670d373ea
criptore <164330862+criptore@users.noreply.github.com>
2024-11-23 19:09:59 +0100

    Create Victoire.c

---
+++ Victoire.c
@@ -0,0 +1,52 @@
+#include <x86gprintrin.h>
+//
+// Created by dariu on 11/23/2024.
+//
+ int victoire(int PlayerX,int PlayerY, int currentPlayer)
+{
+    if (currentPlayer == 1)
+    {
+        if (PlayerX == 8){
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else if (currentPlayer == 2)
+    {
+        if (PlayerX == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else if (currentPlayer == 3)
+    {
+        if (PlayerY == 8)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else if (currentPlayer == 4)
+    {
+        if (PlayerY == 8)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+     return 0;
+
+}
