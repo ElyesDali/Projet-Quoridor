2183e23ef6191017b380e28bdabf6bec2aa7a21a
charlotterousseau <charlotte.rousseau@edu.ece.fr>
2024-11-24 16:32:55 +0100

    Update Victoire.c

--- Victoire.c
+++ Victoire.c
@@ -1,51 +1,15 @@
 //
 // Created by dariu on 11/23/2024.
 //
- int victoire(int PlayerX,int PlayerY, int currentPlayer)
-{
-    if (currentPlayer == 1)
-    {
-        if (PlayerX == 8){
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else if (currentPlayer == 2)
-    {
-        if (PlayerX == 0)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else if (currentPlayer == 3)
-    {
-        if (PlayerY == 8)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    else if (currentPlayer == 4)
-    {
-        if (PlayerY == 8)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-     return 0;
 
+int victoire(int PlayerX, int PlayerY, int currentPlayer) {
+    if (currentPlayer == 1) {
+        return (PlayerX == 8); // Le joueur 1 gagne s'il atteint la ligne 8 (indexé à partir de 0)
+    } else if (currentPlayer == 2) {
+        return (PlayerX == 0); // Le joueur 2 gagne s'il atteint la ligne 0
+    } else if (currentPlayer == 3 || currentPlayer == 4) {
+        return (PlayerY == 8); // Les joueurs 3 et 4 gagnent s'ils atteignent la colonne 8
+    }
+    return 0;
 }
+
