38f76e833ca35bb829fd61936d57d212ababbdc0
ElyesDali <ed09102007@gmail.com>
2024-11-24 23:57:40 +0100

    Add files via upload

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include "sauvegarde.h"
+#include "time.h"
 
 
-void sauvegarder_partie(const char* save, const Partie* partie) {
-    FILE* sauvegarde = fopen("../save.txt", "w");
+int sauvegarder_partie(const char* save, const Partie* partie) {
+    FILE* sauvegarde = fopen("save.txt", "w");
     if (sauvegarde == NULL) {
         printf("Fichier invalide");
         return 0;
     }
-    fprintf(sauvegarde, "%d", partie->tour_actuel);
+    fprintf(sauvegarde, "Joueur actuel: %d\n", partie->joueur_actuel);
+    fprintf(sauvegarde, "Nombre de joueurs: %d\n", partie->nb_joueurs);
 
-    fprintf(sauvegarde, "%d", partie->nb_joueurs);
-
-    for (int i = 0; i < nb_joueurs; i++) {
-        fprintf(sauvegarde, "Pion %d: x: %d; y:%d", i, partie->position_pion[i].x, partie->position_pion[i].y); // On récupère les positions des pions
+    for (int i = 0; i < partie->nb_joueurs; i++) {
+        fprintf(sauvegarde, "Pion %d: x: %d; y: %d\n", i + 1, partie->position_pion[i].x, partie->position_pion[i].y);
     }
 
-    do {
-        fprintf(sauvegarde, "Mur %d: x: %d; y: %d; type: %c",i, partie->position_murs[i].x, partie->position_murs[i].y, partie-> position_murs[i].type); // On récupère les positions de tous les murs placés
-    }while(murs_places[i] != " " );
-
-    for (int i = 0; i < nb_joueurs; i++) {
-        fprintf(sauvegarde, "ID Joueur: %d; Barrières restantes: %d", &partie->donnees[i].player_ID, &partie->donnees[i].remainingBarriers); // On récupère les murs restants de chaque joueur
+    for (int i = 0; i < 30; i++) {
+        if (partie->position_murs[i].type != '\0' && partie->position_murs[i].x >= 0 && partie->position_murs[i].y >= 0) {
+            fprintf(sauvegarde, "Mur %d: x: %d; y: %d; type: %c\n", i + 1, partie->position_murs[i].x, partie->position_murs[i].y, partie->position_murs[i].type);
+        }
     }
 
-    fclose(save);
+    for (int i = 0; i < partie->nb_joueurs; i++) {
+        fprintf(sauvegarde, "Joueur: %d; Barrières restantes: %d\n", i + 1, partie->donnees[i].remainingBarriers);
+    }
+
+    printf("Saving game to save.txt...\n");
+    fclose(sauvegarde);
     return 1;
 }
 
 
 
-void charger_partie(const char* save, Partie* partie) {
-
-    FILE* nv_partie = fopen("../save.txt", "r");
+int charger_partie(const char* save, Partie* partie) {
+    FILE* nv_partie = fopen("save.txt", "r");
 
     if (nv_partie == NULL) {
         printf("Fichier invalide");
@@ -40,26 +42,29 @@ void charger_partie(const char* save, Partie* partie) {
     }
 
 
-    fscanf(nv_partie,"%d", &partie->tour_actuel); // La personne dont c'est le tour
+    fscanf(nv_partie, "Joueur actuel: %d\n", &partie->joueur_actuel);
+    fscanf(nv_partie, "Nombre de joueurs: %d\n", &partie->nb_joueurs);
 
-    for (int i = 0; i < nb_joueurs; i++) {
-        fscanf(nv_partie, "Pion %d: x: %d; y:%d", i, &partie->position_pion[i].x, &partie->position_pion[i].y); // On récupère les positions des pions
+    for (int i = 0; i < partie->nb_joueurs; i++) {
+        fscanf(nv_partie, "Pion %d: x: %d; y: %d\n", &i, &partie->position_pion[i].x, &partie->position_pion[i].y);
     }
 
-    do{
-        fscanf(nv_partie, "Mur %d: x: %d; y: %d; type: %c",i, &partie->position_murs[i].x, &partie->position_murs[i].y, &partie-> position_murs[i].type); // On récupère les positions de tous les murs placés
-        }while(position_murs[i] != "\0");
-
+    int ipm = 0;
+    while (fscanf(nv_partie, "Mur %d: x: %d; y: %d; type: %c\n", &ipm, &partie->position_murs[ipm].x, &partie->position_murs[ipm].y, &partie->position_murs[ipm].type) == 4) {
+        if (partie->position_murs[ipm].x >= 0 && partie->position_murs[ipm].y >= 0) {
+            ipm++;
+            if (ipm >= 30) break; // Limiter à 30 murs
+        }
     }
 
-    for (int i = 0; i < nb_joueurs; i++) {
-        fscanf(nv_partie, "ID Joueur: %d; Barrières restantes: %d", &partie->donnees[i].player_ID, &partie->donnees[i].remainingBarriers); // On récupère les murs restants de chaque joueur
+    for (int i = 0; i < partie->nb_joueurs; i++) {
+        fscanf(nv_partie, "Joueur: %d; Barrières restantes: %d\n", &i, &partie->donnees[i].remainingBarriers);
     }
 
-    fclose(save);
+    fclose(nv_partie);
     return 1;
-
-
-
-
 }
+
+
+
+
--- sauvegarde.h
+++ sauvegarde.h
@@ -1,17 +1,21 @@
 
 #include <stdio.h>
+
+#include "Barriere.h"
+#include "Plato.h"
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
 
 typedef struct {
     Case position_pion[4]; // Les positions de tous les pions
-    Barriere position_murs[50]; // On charge toutes les positions des barrières placées
-    Player donnees[4]; // On récupère les barrières restantes et l'ID du joueur
-    int tour_actuel; // Le joueur dont c'est le tour
+    Barriere position_murs[30]; // On charge toutes les positions des barrières placées
+    Player donnees[4]; // On récupère les barrières restantes
+    int joueur_actuel; // Le joueur dont c'est le tour
+    int nb_joueurs;
 }Partie;
 
-void sauvegarder_partie(FILE* save, const Partie* partie);
-void charger_partie(FILE* save, Partie* partie);
+int sauvegarder_partie(const char* FILE, const Partie* partie);
+int charger_partie(const char* FILE, Partie* partie);
 
 
 #endif //SAUVEGARDE_H
